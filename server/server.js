0e768a90279fe90c31df2df020559ef4bf02f667
Charpell <charpellumeh@gmail.com>
2018-03-27 08:38:53 +0100

    Create route for GET /todos route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var app = express();
 
 app.use(bodyParser.json())
 
+// Post a todo
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -19,6 +20,15 @@ app.post('/todos', (req, res) => {
     res.send(doc)
   }, (e) => {
     res.status(400).send(e)
+  })
+})
+
+// Get all todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos })
+  }, (e) => {
+    res.status(400).send(e);
   })
 })
 
